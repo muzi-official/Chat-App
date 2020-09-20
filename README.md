47eb9508ce3f63d645b4569b02a8215174882ab4
Syed Muzammil Ashfaque <53170318+muzi-official@users.noreply.github.com>
2020-09-20 17:51:56 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Chat-App
-chat app
+# chatApp
---
+++ firebase.init.js
@@ -0,0 +1,15 @@
+﻿
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyABNx0V_Rzq-cMjFmsTBTwAPZAwnu2uLjA",
+    authDomain: "wechat-85b94.firebaseapp.com",
+    databaseURL: "https://wechat-85b94.firebaseio.com",
+    projectId: "wechat-85b94",
+    storageBucket: "wechat-85b94.appspot.com",
+    messagingSenderId: "533476222154",
+    appId: "1:533476222154:web:2fc16be4755724f3920033",
+    measurementId: "G-6LJWP43E4T"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
